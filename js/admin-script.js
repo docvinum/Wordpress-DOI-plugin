0b4318084a1cf692e534e859ac24195941c80f2c
Alexandre Basta <42250730+docvinum@users.noreply.github.com>
2023-04-09 21:03:33 +0000

    DOI popup

--- js/admin-script.js
+++ js/admin-script.js
@@ -23,3 +23,28 @@ document.addEventListener('DOMContentLoaded', function () {
         saveButton.style.backgroundColor = '#34a853';
     }
 });
+
+
+jQuery(document).ready(function($) {
+    $(document).on('click', '.wp-crossref-doi-button', function(e) {
+        e.preventDefault();
+
+        var postId = $(this).data('post-id');
+
+        var $dialog = $('<div></div>').html('<iframe style="border: 0;" src="about:blank" width="100%" height="100%"></iframe>').dialog({
+            autoOpen: false,
+            modal: true,
+            height: 625,
+            width: 500,
+            title: 'Crossref DOI',
+            close: function() {
+                $dialog.dialog('destroy').remove();
+            }
+        });
+
+        var targetUrl = wp_crossref_doi_vars.wp_crossref_doi_ajaxurl + '?action=wp_crossref_doi_edit_page&post_id=' + postId + '&_wpnonce=' + wp_crossref_doi_vars.wp_crossref_doi_nonce;
+
+        $dialog.find('iframe').attr('src', targetUrl);
+        $dialog.dialog('open');
+    });
+});
